f36edd7bb359172169cae248b22d96dfe8495dc8
Yash UNHAWANE <74054179+yashunhawane@users.noreply.github.com>
2024-08-07 17:51:22 +0000

    flipdeal

---
+++ flipDeal.js
@@ -0,0 +1,74 @@
+let express = require("express");
+let cors = require("cors");
+let app = express();
+app.use(cors());
+
+let taxRate = 5;
+let discountPercentage = 10;
+let loyaltyRate = 2;
+
+//Endpoint 1: Calculate the total price of items in the cart
+
+app.get("/cart-total", (req, res) => {
+  let newItemPrice = parseFloat(req.query.newItemPrice);
+  let cartTotal = parseInt(req.query.cartTotal);
+  let total = newItemPrice + cartTotal;
+  res.send(total.toString());
+});
+
+//Endpoint 2 : Apply a discount based on membership status
+
+app.get("/membership-discount", (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember;
+  let discount;
+  if (isMember === "true") {
+    discount = (cartTotal * discountPercentage) / 100;
+  } else {
+    discount = 0;
+  }
+  res.send(discount.toString());
+});
+
+//Endpoint 3 : Calculate tax on the cart total
+
+app.get("/calculate-tax", (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let tax = (cartTotal * taxRate) / 100;
+  res.send(tax.toString());
+});
+
+//Endpoint 4 : Estimate delivery time based on shipping method
+app.get("/estimate-delivery", (req, res) => {
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+  let deliveryTime;
+  if (shippingMethod === "standard") {
+    deliveryTime = parseInt(distance / 50);
+  } else {
+    deliveryTime = parseInt(distance / 100);
+  }
+  res.send(deliveryTime.toString());
+});
+
+//Endpoint 5 : Calculate the shipping cost based on weight and distance
+app.get("/shipping-cost", (req, res) => {
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+  let shipppingCost = parseInt(weight * distance * 0.1);
+  res.send(shipppingCost.toString());
+});
+
+//Endpoint 6 : Calculate loyalty points earned from a purchase
+
+app.get("/loyalty-points", (req, res) => {
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+  let loyaltyPoints = parseInt(purchaseAmount * loyaltyRate);
+  res.send(loyaltyPoints.toString());
+});
+
+let PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
